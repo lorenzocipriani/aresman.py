dfbca68ad4a032c71fe546d9ba4314b41dc7ed7a
lorenzocipriani <lorenzo1974@gmail.com>
2017-11-09 19:37:36 +0100

    Added cpustat

--- src/aresman.py
+++ src/aresman.py
@@ -9,9 +9,11 @@ aresman.py
 
 '''
 
+import os
 import time
 
-poll_interval = 10
+USER_HZ = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+poll_interval = 5
 
 def cpuinfo():
     '''
@@ -33,6 +35,44 @@ def cpuinfo():
                 cpus[int(processor)] = cpu
     return cpus
 
+def toSecs(data):
+    '''
+    '''
+    return float(data)/USER_HZ
+
+def cpustat(data):
+    '''
+    '''
+    cpu = {}
+    cpu["id"] = data[0].strip()
+    cpu["user"] = data[1].strip()
+    cpu["nice"] = data[2].strip()
+    cpu["system"] = data[3].strip()
+    cpu["idle"] = data[4].strip()
+    cpu["iowait"] = data[5].strip()
+    cpu["irq"] = data[6].strip()
+    cpu["softirq"] = data[7].strip()
+    cpu["steal"] = data[8].strip()
+    cpu["guest"] = data[9].strip()
+    cpu["guest_nice"] = data[10].strip()
+    return cpu
+
+
+def stat():
+    '''
+    '''
+    labels = ["cpu", "btime", "processes", "procs_running", "procs_blocked"]
+    cpus = []
+    stat = {}
+    
+    with open('/proc/stat') as f:
+        for line in f:
+            myLine = line.split(' ')
+            if myLine[0].strip() in labels:
+                if "cpu" in myLine[0].strip(): cpus.append(cpustat(myLine))
+                else: stat[myLine[0].strip()] = myLine[1].strip()
+    return stat
+
 
 def main():
     '''
@@ -45,6 +85,11 @@ def main():
         
         while True:
             
+            stats = stat()
+            print("uptime: {} sec\tprocs: {} ({} running, {} blocked)".format(toSecs(stats["btime"]), stats["processes"], stats["procs_running"], stats["procs_blocked"]))
+            
+            for cpu in stats["cpu"]:
+                print("id: {}\tuser: {}\tnice: {}\tsystem: {}\tidle: {}\twait: {}".format(stats["id"], toSecs(stats["user"], toSecs(stats["nice"]), toSecs(stats["system"]), toSecs(stats["idle"]), toSecs(stats["wait"]))))
             
             time.sleep(poll_interval)
         
