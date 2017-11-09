bfd04d9a252fd482f777b69b166168fa56fa2c7a
lorenzocipriani <lorenzo1974@gmail.com>
2017-11-09 16:47:42 +0100

    split line

--- src/aresman.py
+++ src/aresman.py
@@ -18,8 +18,8 @@ def cpuinfo():
     '''
     with open('/proc/cpuinfo') as f:
         for line in f:
-            myLine = ' '.join(line.split())
-            print(myLine)
+            print(line.rstrip('\n'))
+
 
 def main():
     '''
