c4a674c994d083432fcab6fdbd2df6da420dfee4
lorenzocipriani <lorenzo1974@gmail.com>
2017-11-09 21:54:54 +0100

    Load test routine

---
+++ src/primes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+'''
+primes.py
+@author: Lorenzo Cipriani <lorenzo1974@gmail.com>
+@contact: https://www.linkedin.com/in/lorenzocipriani
+@since: 2017-10-23
+@see:
+
+'''
+
+primesToFind = 1000000
+
+num = 0
+
+def isPrime(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0: return False
+        else: return True
+    else: return False
+    
+    
+if __name__ == '__main__':
+    
+    print("Search for the first {} prime numbers:".format(primesToFind))
+    
+    while primesToFind > 0:
+        num += 1
+        if isPrime(num):
+            print(num)
+            primesToFind -= 1
